391122114cac97a72276e7299c8f669f2a107800
DeniMiljevic <miljevicdeni@gmail.com>
2026-03-29 19:27:16 +0200

    Added get i to

--- AbySalto.Junior/Controllers/RestaurantController.cs
+++ AbySalto.Junior/Controllers/RestaurantController.cs
@@ -1,8 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AbySalto.Junior.DTO;
+using AbySalto.Junior.Enums;
+using AbySalto.Junior.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AbySalto.Junior.Controllers
 {
-    public class RestaurantController : Controller
+    [Route("api/restaurant")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
     {
+        private readonly IOrderService _orderService;
+
+        public RestaurantController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        [Route("orders")]
+        public async Task<IActionResult> Create([FromBody] CreateOrderDto order, CancellationToken ct)
+        {
+            if (order == null) return BadRequest("Order data is required.");
+            try
+            {
+                await _orderService.Create(order, ct);
+                return Ok("Order created successfully.");
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while creating the order.");
+            }
+        }
+
+        [HttpGet]
+        [Route("orders")]
+        public async Task<IActionResult> GetAll([FromQuery] bool orderByTotal, [FromQuery] bool descending, CancellationToken ct)
+        {
+            try
+            {
+                var orders = await _orderService.GetAll(orderByTotal, descending, ct);
+                return Ok(orders);
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while retrieving orders.");
+            }
+        }
+
+        [HttpPatch]
+        [Route("orders/{orderId}/status")]
+        public async Task<IActionResult> Update([FromRoute] int orderId, [FromBody] UpdateOrderDto updateOrderDto, CancellationToken ct)
+        {
+            try
+            {
+                await _orderService.Update(orderId, updateOrderDto, ct);
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, "An error occurred while creating the order.");
+            }
+        }
+
+        [HttpGet("orders/{id}/total")]
+        public async Task<IActionResult> GetTotal(int id, CancellationToken ct)
+        {
+            var total = await _orderService.GetOrderTotalAsync(id, ct);
+            return Ok(new { total });
+        }
     }
 }
--- AbySalto.Junior/Infrastructure/Database/IApplicationDbContext.cs
+++ AbySalto.Junior/Infrastructure/Database/IApplicationDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace AbySalto.Junior.Infrastructure.Database
+﻿using Microsoft.EntityFrameworkCore;
+using AbySalto.Junior.Models;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AbySalto.Junior.Infrastructure.Database
 {
     public interface IApplicationDbContext
     {
+        DbSet<Order> Orders { get; set; }
+        DbSet<OrderItem> OrderItems { get; set; }
+        DbSet<Item> Items { get; set; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
